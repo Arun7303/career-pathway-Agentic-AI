85064e2001523455a37e40a982a132cc7f8e7860
Arun Adhikari <53912688+Arun7303@users.noreply.github.com>
2026-02-26 20:17:28 +0530

    generic placeholder
    Updated GOOGLE_API_KEY placeholder for security.

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-123'
-    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY') or 'AIzaSyCNUHTC85Rs5eryzP5hlFC3bcU0DTejm5s'  # Replace with new key soon!
-    MODEL_NAME = 'gemini-2.0-flash'
+    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY') or 'Your_API_Key'  # Replace with new key soon!
+    MODEL_NAME = 'gemini-2.0-flash' #change model accordingly to your need
     
 class DevelopmentConfig(Config):
     DEBUG = True
